cf9c76cfd4ace18b7202445967acdbb0c6268378
Kiyohito Kunii <kk3317@line05.doc.ic.ac.uk>
2018-06-14 14:54:35 +0100

    update gym

--- GVGAI_GYM/practice.py
+++ GVGAI_GYM/practice.py
@@ -1,4 +1,40 @@
 import gym
 import gym_gvgai
 
-env = gym.make("aliens-gvgai-v0")
+import matplotlib.pyplot as plt
+from IPython import display
+from controller import keyboard_activate
+
+
+def show_state(env, step=0, name="", info=""):
+    plt.figure(3)
+    plt.clf()
+    plt.imshow(env.render(mode="rgb_array"))
+    plt.title("%s | Steap %d %s" % (name, step, info))
+    plt.axis("off")
+
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+
+# for env in gym.envs.registry.all():
+#     if env.id.startswith('gvgai'):
+#         print(env.id)
+
+# env = gym.make("gvgai-aliens-lvl0-v0")
+env = gym.make("gvgai-aaa-lvl0-v0")
+
+env.reset()
+sum_score = 0
+
+for i in range(2000):
+    show_state(env, i, "Aliens", str(sum_score))
+    # action_id = env.action_space.sample()
+    action_id = keyboard_activate()
+    state, reward, isOver, debug = env.step(action_id)
+    sum_score += reward
+
+    print("Action " + str(action_id) + " played at t=", str(i+1) + ", reward=" \
+            + str(reward) + ", new score=", sum_score)
+    if isOver:
+        print("Game terminates at t=" + str(i+1))
+        break
--- README.md
+++ README.md
@@ -1,40 +1,18 @@
 # Individual_project
 Imperial College London MSc Individual Project
 
-
-
-
-
-## Schedule
-
-
 ## ToDo
+- Update Background report
+- Check References
+- Make a benchmarks (Q-learning and DRL)
+- Berkeley DRL
+- Math for ML
 
-### May
-Main
-- Write background report
-- Learn basic technology (Reinforcement learning and DRL, Math for Machine Learning)
-- Decide experimental platform
-- Start coding for the baseline performance
-
-Learning
-- Finish RL
-- Start DRL
-- Finish first part of Math for ML
-
-### June to mid-July
-- Finish Math for ML
-- Start DL
-
-
-###  end of June
-
-Simple game
 
 ### July Extension
 
 Extension
-### Half august 
+### Half august
 Evaluation
 ### Mid-July to Mid-August
 - Finish Writing the report
