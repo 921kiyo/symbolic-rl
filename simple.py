9edf6201f9166a7ba881246c231b54d06895e79d
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-07-19 16:21:56 +0100

    add stuff in simple.py

--- simple.py
+++ simple.py
@@ -4,6 +4,9 @@ import gym_vgdl
 import numpy as np
 import time
 
+import os
+import subprocess
+
 TIME_RANGE = 400
 env = gym.make('vgdl_zelda_features-v0')
 print(env.unwrapped.observer.get_observation())
@@ -25,3 +28,27 @@ for i_episode in range(TIME_RANGE):
             break
 
 
+
+print("ILASP running...")
+dir = os.path.dirname(os.path.abspath(__file__))
+dir = os.path.join(dir, 'las_log/cache.las')
+cache = "--cached-rel=" + dir
+
+filename = "output.las"
+try:
+    # Hardcode
+    # hypothesis = "state_after(V0) :- adjacent(right, V0, V1), state_before(V1), action(right), not wall(V0).\nstate_after(V0) :- adjacent(left, V0, V1), state_before(V1), action(left), not wall(V0).\nstate_after(V0) :- adjacent(down, V0, V1), state_before(V1), action(down), not wall(V0).\nstate_after(V0) :- adjacent(up, V0, V1), state_before(V1), action(up), not wall(V0)."    
+    # Clingo 5
+    clingo5 = "'clingo5 --opt-strat=usc,stratify'"
+    # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-q", "--clingo5", "--clingo", clingo5], universal_newlines=True)
+    
+    # Normal 
+    hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", "-q"], universal_newlines=True)
+    
+    # Cache
+    # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", "-q", cache], universal_newlines=True)
+except subprocess.CalledProcessError as e:
+    print("Error...", e.output)
+    hypothesis = e.output
+
+print(hypothesis)
