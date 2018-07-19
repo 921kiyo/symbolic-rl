04c3821269f024dfd6988fc579e45cce1eb562b2
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-07-19 12:23:38 +0100

    working on updating vgdl

---
+++ simple.py
@@ -0,0 +1,27 @@
+import matplotlib
+import gym
+import gym_vgdl
+import numpy as np
+import time
+
+TIME_RANGE = 400
+env = gym.make('vgdl_zelda_features-v0')
+print(env.unwrapped.observer.get_observation())
+print(list(env.unwrapped.observer.get_observation()))
+
+for i_episode in range(TIME_RANGE):
+
+    # Reset the env and pick the first action
+    state = env.reset()
+
+    for t in range(TIME_RANGE):
+        env.render()
+        time.sleep(0.1)
+        # Take a step
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+
+        if done:
+            break
+
+
