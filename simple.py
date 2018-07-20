8381a6b94ce675644e424e30e9900c0826a9cbc7
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-07-20 14:35:45 +0100

    update simple, subprocess working now in simple.py

--- simple.py
+++ simple.py
@@ -7,6 +7,8 @@ import time
 import os
 import subprocess
 
+from lib.helper import _gen_log_dir
+
 TIME_RANGE = 400
 env = gym.make('vgdl_zelda_features-v0')
 print(env.unwrapped.observer.get_observation())
@@ -27,28 +29,30 @@ for i_episode in range(TIME_RANGE):
         if done:
             break
 
+# print("ILASP running...")
+# dir = os.path.dirname(os.path.abspath(__file__))
+# # dir = os.path.join(dir, 'las_log/cache.las')
+# # cache = "--cached-rel=" + dir
+# print("dir ", dir)
+# print(_gen_log_dir(dir, prefix="a"))
 
+# # ILASP --version=2i output2.las -ml=10 --clingo5 --clingo "clingo5 --opt-strat=usc,stratify"
 
-print("ILASP running...")
-dir = os.path.dirname(os.path.abspath(__file__))
-dir = os.path.join(dir, 'las_log/cache.las')
-cache = "--cached-rel=" + dir
-
-filename = "output.las"
-try:
-    # Hardcode
-    # hypothesis = "state_after(V0) :- adjacent(right, V0, V1), state_before(V1), action(right), not wall(V0).\nstate_after(V0) :- adjacent(left, V0, V1), state_before(V1), action(left), not wall(V0).\nstate_after(V0) :- adjacent(down, V0, V1), state_before(V1), action(down), not wall(V0).\nstate_after(V0) :- adjacent(up, V0, V1), state_before(V1), action(up), not wall(V0)."    
-    # Clingo 5
-    clingo5 = "'clingo5 --opt-strat=usc,stratify'"
-    # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-q", "--clingo5", "--clingo", clingo5], universal_newlines=True)
+# filename = "output2.las"
+# try:
+#     # Hardcode
+#     # hypothesis = "state_after(V0) :- adjacent(right, V0, V1), state_before(V1), action(right), not wall(V0).\nstate_after(V0) :- adjacent(left, V0, V1), state_before(V1), action(left), not wall(V0).\nstate_after(V0) :- adjacent(down, V0, V1), state_before(V1), action(down), not wall(V0).\nstate_after(V0) :- adjacent(up, V0, V1), state_before(V1), action(up), not wall(V0)."    
+#     # Clingo 5
+#     clingo5 = "clingo5 --opt-strat=usc,stratify"
+#     hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "--clingo5", "--clingo", clingo5], universal_newlines=True)
     
-    # Normal 
-    hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", "-q"], universal_newlines=True)
+#     # Normal 
+#     # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", ], universal_newlines=True)
     
-    # Cache
-    # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", "-q", cache], universal_newlines=True)
-except subprocess.CalledProcessError as e:
-    print("Error...", e.output)
-    hypothesis = e.output
+#     # Cache
+#     # hypothesis = subprocess.check_output(["ILASP", "--version=2i", filename, "-ml=10", "-nc", cache], universal_newlines=True)
+# except subprocess.CalledProcessError as e:
+#     print("Error...", e.output)
+#     hypothesis = e.output
 
-print(hypothesis)
+# print(hypothesis)
