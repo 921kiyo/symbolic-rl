31bf9745794c6424a29462d9e92220ae70b081bc
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-09-01 08:51:23 +0100

    update report

---
+++ report/experiment1_hypothesis.pl
@@ -0,0 +1,16 @@
+state_after(V1) :- adjacent(right, V0, V1), state_before(V1),
+                   action(right), wall(V0).
+state_after(V0) :- adjacent(right, V0, V1), state_before(V0),
+                   action(left), wall(V1).
+state_after(V1) :- adjacent(down, V0, V1), state_before(V1),
+                   action(down), wall(V0).
+state_after(V1) :- adjacent(up, V0, V1), state_before(V1),
+                   action(up), wall(V0).
+state_after(V0) :- adjacent(right, V0, V1), state_before(V1),
+                   action(right), not wall(V0).
+state_after(V0) :- adjacent(left, V0, V1), state_before(V1),
+                   action(left), not wall(V0).
+state_after(V0) :- adjacent(down, V0, V1), state_before(V1),
+                   action(down), not wall(V0).
+state_after(V0) :- adjacent(up, V0, V1), state_before(V1),
+                   action(up), not wall(V0).
