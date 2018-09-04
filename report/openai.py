fb352b1004940e6ba7e564824494a4809b8379b2
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-09-04 04:20:44 +0100

    env platform is finished

--- report/openai.py
+++ report/openai.py
@@ -1,12 +1,24 @@
   import gym # import OpenAI gym package
   import gym_vgdl # used to connect VGDL and OpenAI gym
 
-  env = gym.make('VDGL_ENVNAME') # initialise an instance of a game
-  env.reset()
-  action = 0 # integer between 0 and 3
-  # 0: Up
-  # 1: Down
-  # 2: Left
-  # 3: Right
-  next_state, reward, done, _ = env.step(action) # take an action and get an observation
-  env.render() # update the frame of the environment (for visualisation purpose)
+  num_episodes = 100 # num_episodes is specified by users
+  time_steps = 100 # time_steps is specified by users
+
+  # initialise an instance of a VDGL game
+  env = gym.make('VDGL_ENVNAME')
+
+  for i_episode in range(num_episodes):
+      env.reset()  # the agent starts from a starting point
+      for t in range(time_steps):
+          action = 0 # an integer between 0 and 3
+                     # and is chosen by your RL algorithm.
+                     # Action 0: Up, 1: Down, 2: Left, 3: Right
+
+          # take an action and get an observation
+          next_state, reward, done, _ = env.step(action)
+          env.render() # update the frame of the environment
+
+          # when done is True, the agent is at a terminal state
+          # and the current episode is finished
+          if done:
+              break
